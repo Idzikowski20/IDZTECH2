8a38e11eadbc3d11c97b3586b28c8f8e8dfc3a82
Idzikowski20 <patryk.idzikowski@interia.pl>
2025-05-22 16:11:15 +0200

    .

--- server.cjs
+++ server.cjs
@@ -7,9 +7,16 @@ const app = express();
 
 // Dodaj obsługę CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  const allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5173', 'https://idztech.onrender.com'];
+  const origin = req.headers.origin;
+  
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
@@ -297,5 +304,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Serwer działa! ❤️`);
 });
